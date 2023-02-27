8e39fa32eeff33b70fc041f69f163dc13a00b75e
Youssef Mohammed <99803110+YoussefmSaber@users.noreply.github.com>
2023-02-27 17:18:05 +0200

    first commit
    Add the main 3 pages and started on note item design

---
+++ app/src/main/java/com/example/noteapp/Fragment/UpdateNotesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.noteapp.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.noteapp.R
+
+class UpdateNotesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_notes, container, false)
+    }
+}
