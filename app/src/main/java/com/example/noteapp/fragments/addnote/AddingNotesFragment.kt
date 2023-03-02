aab630c1f4b1224f90d0078b2fc7b30f6bba743e
youssef mohammed <youssef.mu.saber@gmail.com>
2023-03-02 22:51:47 +0200

    Added a functinality to change the color of the marker

--- app/src/main/java/com/example/noteapp/fragments/addnote/AddingNotesFragment.kt
+++ app/src/main/java/com/example/noteapp/fragments/addnote/AddingNotesFragment.kt
@@ -5,7 +5,9 @@ import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -21,6 +23,8 @@ class AddingNotesFragment : Fragment() {
 
     private lateinit var _noteViewModel: NoteViewModel
 
+    var colorChoosen: String = ""
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +33,26 @@ class AddingNotesFragment : Fragment() {
         _binding = FragmentAddingNotesBinding.inflate(layoutInflater)
 
         _noteViewModel = ViewModelProvider(this)[NoteViewModel::class.java]
+
+        _binding?.blueColor?.setOnClickListener {
+            setNoteColor("blue", R.drawable.light_blue_mark_selected, it as ImageView)
+        }
+
+        _binding?.greenColor?.setOnClickListener {
+            setNoteColor("green", R.drawable.light_green_mark_selected, it as ImageView)
+        }
+
+        _binding?.pinkColor?.setOnClickListener {
+            setNoteColor("pink", R.drawable.light_pink_mark_selected, it as ImageView)
+        }
+
+        _binding?.orangeColor?.setOnClickListener {
+            setNoteColor("orange", R.drawable.light_orange_mark_selected, it as ImageView)
+        }
+
+        _binding?.purpleColor?.setOnClickListener {
+            setNoteColor("purple", R.drawable.light_purple_mark_selected, it as ImageView)
+        }
 
         _binding?.saveNoteBtn?.setOnClickListener {
             insertDataToDatabase()
@@ -40,7 +64,7 @@ class AddingNotesFragment : Fragment() {
     private fun insertDataToDatabase() {
         val title = _binding?.newNoteTitleText?.text.toString()
         val detail = _binding?.newNoteDetailText?.text.toString()
-        val color = "Green"
+        val color = colorChoosen
 
         if (inputCheck(title, detail, color)) {
             val note = Note(0, title, detail, color)
@@ -50,8 +74,34 @@ class AddingNotesFragment : Fragment() {
             Toast.makeText(requireContext(), "Successfully add the note", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.AddingNote2MainPage)
         } else {
-            Toast.makeText(requireContext(), "Please check you checked everything", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                requireContext(),
+                "Please check you checked everything",
+                Toast.LENGTH_SHORT
+            ).show()
         }
+    }
+
+    private fun setNoteColor(color: String, colorResource: Int, selectedView: ImageView) {
+        colorChoosen = color
+        _binding?.blueColor?.setImageResource(R.drawable.light_blue_mark_notselected)
+        _binding?.greenColor?.setImageResource(R.drawable.light_green_mark_notselected)
+        _binding?.pinkColor?.setImageResource(R.drawable.light_pink_mark_notselected)
+        _binding?.orangeColor?.setImageResource(R.drawable.light_orange_mark_notselected)
+        _binding?.purpleColor?.setImageResource(R.drawable.light_purple_mark_notselected)
+        selectedView.setImageResource(colorResource)
+        _binding?.titleBackgroundColor?.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(), when (colorChoosen) {
+                    "blue" -> R.color.light_blue
+                    "green" -> R.color.light_green
+                    "pink" -> R.color.light_pink
+                    "purple" -> R.color.light_purple
+                    "orange" -> R.color.light_orange
+                    else -> R.color.white
+                }
+            )
+        )
     }
 
     private fun inputCheck(title: String, detail: String, color: String): Boolean {
--- app/src/main/java/com/example/noteapp/utils/BindingUtils.kt
+++ app/src/main/java/com/example/noteapp/utils/BindingUtils.kt
@@ -1,7 +1,9 @@
 package com.example.noteapp.utils
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.example.noteapp.R
 import com.example.noteapp.model.Note
 
 @BindingAdapter("NoteTitle")
@@ -15,5 +17,19 @@ fun TextView.setNoteTitle(item: Note?) {
 fun TextView.setNoteDetail(item: Note?) {
     item?.let {
         text = item.noteDescription
+    }
+}
+
+@BindingAdapter("MarkColor")
+fun ImageView.setCurserColor(item: Note?) {
+    item?.let {
+        setImageResource(when(item.noteColor){
+            "blue" -> R.drawable.light_blue_mark
+            "green" -> R.drawable.light_green_mark
+            "pink" -> R.drawable.light_pink_mark
+            "orange" -> R.drawable.light_orange_mark
+            "purple" -> R.drawable.light_purple_mark
+            else -> R.drawable.light_green_mark
+        })
     }
 }
